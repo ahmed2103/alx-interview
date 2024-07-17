e6d7c7e57cecb2b93f1762bf277bf381e922e3ea
Hasona <bekoahmed20@gmail.com>
2024-07-17 12:19:59 +0300

    why signals if there is ready exception? regex is a proplem in itselg.

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Script to do some statistics calculations and handles exceptions"""
+
+import sys
+
+
+def print_stats(size, status_codes):
+    """Function prints precalculated statistics"""
+    print("File size:", size)
+    for code in sorted(status_codes.keys()):  # to gurantee order
+        if status_codes[code] > 0:
+            print("{}: {}".format(code, status_codes[code]))
+
+
+def main():
+    """Main function of the script logic"""
+    status_dict = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    line_count = 0
+    total_size = 0
+
+    try:
+        for line in sys.stdin:
+            line_count += 1
+            try:
+                parts = line.split(" ")
+                size = int(parts[-1])
+                status = int(parts[-2])
+
+                total_size += size
+                if status in status_dict:
+                    status_dict[status] = status_dict[status] + 1
+            except (IndexError, ValueError):
+                pass
+            if line_count % 10 == 0:
+                print_stats(total_size, status_dict)
+    except KeyboardInterrupt:
+        print_stats(total_size, status_dict)
+        raise
+
+    print_stats(total_size, status_dict)  # if ended normally
+
+
+if __name__ == '__main__':
+    main()
---
+++ 0x03-log_parsing/README.md
@@ -0,0 +1,22 @@
+this is readme file for alx interview task
+this is simple string parser that handles specific string parse it and do
+some statistics.
+the stdin cource generated from this script
+#!/usr/bin/python3
+import random
+import sys
+from time import sleep
+import datetime
+
+for i in range(10000):
+    sleep(random.random())
+    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
+        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
+        datetime.datetime.now(),
+        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+        random.randint(1, 1024)
+    ))
+    sys.stdout.flush()
+
+
+then piped ./0-generator.py | ./0-stats.py 
