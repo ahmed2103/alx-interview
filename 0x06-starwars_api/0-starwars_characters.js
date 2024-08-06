d4df55cae801b184bf4fd477016010aaeef5860a
Hasona <bekoahmed20@gmail.com>
2024-08-06 23:43:06 +0300

    why JS if not async!

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,22 +4,37 @@ get Star Wars characters from the Star Wars API
  */
 
 const request = require('request');
-const process = require('process');
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
 
-const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
+if (isNaN(process.argv[2])) {
+  process.exit();
+}
 
 request(url, (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
-    JSON.parse(body).characters.forEach(url => {
-      request(url, (error, response, body) => {
-        if (error) {
-          console.error(error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
+    charactersUrls = JSON.parse(body).characters;
+    characterNames = charactersUrls.map((url) => {
+      return new Promise((resolve, reject) => {
+        request(url, (error, response, body) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(body).name);
+          }
+        });
       });
     });
+
+    Promise.all(characterNames)
+      .then((names) => {
+        names.forEach((name) => {
+          console.log(name);
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 });
