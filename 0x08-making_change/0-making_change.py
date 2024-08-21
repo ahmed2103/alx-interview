baa74daa7fc9b4d50c7d86ad73ab1811baf36485
Hasona <bekoahmed20@gmail.com>
2024-08-21 19:28:31 +0300

    Not every time memoize

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -12,6 +12,7 @@ def makeChange(coins, total):
 
     for i in range(1, total + 1):
         for coin in coins:
-            dp[i] = min(dp[i], dp[i - coin] + 1)
+            if i >= coin:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
 
     return dp[total] if dp[total] != float('inf') else -1
