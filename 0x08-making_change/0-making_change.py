e0a3673d064abeea4bbe3a1083b489425d51f77a
Hasona <bekoahmed20@gmail.com>
2024-08-21 21:05:48 +0300

    Not every time memoize

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,13 +6,21 @@ def makeChange(coins, total):
     """Minimum ways to make coin change module buttom up approach"""
     if total <= 0:
         return 0
+    memo = dict()
 
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
-    for i in range(1, total + 1):
+    def dp(amount):
+        """helper function"""
+        if amount in memo:
+            return memo[amount]
+        if amount == 0:
+            return 0
+        min_coins = float('inf')
         for coin in coins:
-            if i >= coin:
-                dp[i] = min(dp[i], dp[i - coin] + 1)
+            if coin <= amount:
+                min_coins = min(min_coins, dp(amount - coin) + 1)
 
-    return dp[total] if dp[total] != float('inf') else -1
+        memo[amount] = min_coins
+        return min_coins
+
+    result = dp(total)
+    return result if result != float('inf') else - 1
