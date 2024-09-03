56ab8698a3c266f6bd9e7ef0b67f5134ad01e377
Hasona <bekoahmed20@gmail.com>
2024-09-03 15:32:18 +0300

    sum for counting as if we get the length of the primes

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,34 @@
+def isWinner(x, nums):
+    def sieve_of_eratosthenes(n):
+        primes = [True] * (n + 1)
+        primes[0] = primes[1] = False
+        for i in range(2, int(n**0.5) + 1):
+            if primes[i]:
+                for j in range(i*i, n+1, i):
+                    primes[j] = False
+        return primes
+
+    def determine_winner(n):
+        primes = sieve_of_eratosthenes(n)
+        prime_count = max(primes)
+        return "Maria" if prime_count % 2 == 1 else "Ben"
+
+    if not nums or x != len(nums):
+        return None
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        winner = determine_winner(n)
+        if winner == "Maria":
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
